4677531c2647e751896c464b64e8e1a26a7158d8
epic3azoozez135 <44898336+epic3azoozez135@users.noreply.github.com>
2018-11-11 22:55:12 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,13 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const client2 = new Discord.Client();
 
-client.on('ready', async() => {
+client2.on('ready', async() => {
 var server = "509370079046270977"; // ايدي السررفر
 var channel = "509708477854384138";//ايدي الروم
     setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('**- SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM **')
+    client2.guilds.get(server).channels.get(channel).send('- SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM ')
     },305);
 })
 
 
-client.login(process.env.BOT_TOKEN);
+client2.login(process.env.BOT_TOKEN2);

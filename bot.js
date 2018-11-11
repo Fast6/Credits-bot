293f887f3a9209ef01d3c0779e145d0d209537bd
epic3azoozez135 <44898336+epic3azoozez135@users.noreply.github.com>
2018-11-11 22:49:39 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,27 @@
 const Discord = require('discord.js');
-const client5 = new Discord.Client();
+const client = new Discord.Client();
 
-client5.on('ready', async() => {
+client.on('ready', async() => {
 var server = "509370079046270977"; // ايدي السررفر
 var channel = "509708477854384138";//ايدي الروم
     setInterval(()=>{
-    client5.guilds.get(server).channels.get(channel).send('**- SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM **')
+    client.guilds.get(server).channels.get(channel).send('**- SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM **')
     },305);
 })
 
 
-client5.login(process.env.BOT_TOKEN5);
+client.login(process.env.BOT_TOKEN);
+
+const Discord = require('discord.js');
+const client2 = new Discord.Client();
+
+client2.on('ready', async() => {
+var server = "509370079046270977"; // ايدي السررفر
+var channel = "509708477854384138";//ايدي الروم
+    setInterval(()=>{
+    client2.guilds.get(server).channels.get(channel).send('**- SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM **')
+    },305);
+})
+
+
+client2.login(process.env.BOT_TOKEN2);

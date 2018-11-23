050214c004732111eb8025e36815b46e29433747
epic3azoozez135 <44898336+epic3azoozez135@users.noreply.github.com>
2018-11-23 17:38:31 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,18 +4,6 @@ const client2 = new Discord.Client();
 const client3 = new Discord.Client();
 const client4 = new Discord.Client();
 const client5 = new Discord.Client();
-const client6 = new Discord.Client();
-const client7 = new Discord.Client();
-const client8 = new Discord.Client();
-const client9 = new Discord.Client();
-const client10 = new Discord.Client();
-const client11 = new Discord.Client();
-const client12 = new Discord.Client();
-const client13 = new Discord.Client();
-const client14 = new Discord.Client();
-const client15 = new Discord.Client();
-const client16 = new Discord.Client();
-
 
 
 
@@ -36,7 +24,7 @@ var channel = "515230042536804353";//ايدي الروم
 })
 client3.on('ready', async() => {
 var server = "515229161804267520"; // ايدي السررفر
-var channel = "";//ايدي الروم
+var channel = "515230042536804353";//ايدي الروم
     setInterval(()=>{
     client3.guilds.get(server).channels.get(channel).send(' -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee')
     },305);
@@ -54,84 +42,6 @@ var channel = "515230042536804353";//ايدي الروم
     setInterval(()=>{
     client5.guilds.get(server).channels.get(channel).send(' -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee')
     },305);
-})
-client6.on('ready', async() => {
-var server = "509370079046270977"; // ايدي السررفر
-var channel = "511579498991583252";//ايدي الروم
-    setInterval(()=>{
-    client6.guilds.get(server).channels.get(channel).send(' -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee')
-    },305);
-})
-client7.on('ready', async() => {
-var server = "509370079046270977"; // ايدي السررفر
-var channel = "511579498991583252";//ايدي الروم
-    setInterval(()=>{
-    client7.guilds.get(server).channels.get(channel).send(' -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee')
-    },305);
-})
-client8.on('ready', async() => {
-var server = "509370079046270977"; // ايدي السررفر
-var channel = "511579498991583252";//ايدي الروم
-    setInterval(()=>{
-    client8.guilds.get(server).channels.get(channel).send(' -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee')
-    },305);
-})
-client9.on('ready', async() => {
-var server = "509370079046270977"; // ايدي السررفر
-var channel = "511597873222123521";//ايدي الروم
-    setInterval(()=>{
-    client9.guilds.get(server).channels.get(channel).send(' -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee')
-    },305);
-})
-client10.on('ready', async() => {
-var server = "509370079046270977"; // ايدي السررفر
-var channel = "511597873222123521";//ايدي الروم
-    setInterval(()=>{
-    client10.guilds.get(server).channels.get(channel).send(' -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee')
-    },305);
-})
-client11.on('ready', async() => {
-var server = "509370079046270977"; // ايدي السررفر
-var channel = "511597873222123521";//ايدي الروم
-    setInterval(()=>{
-    client11.guilds.get(server).channels.get(channel).send(' -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee')
-    },305);
-})
-client12.on('ready', async() => {
-var server = "509370079046270977"; // ايدي السررفر
-var channel = "511597873222123521";//ايدي الروم
-    setInterval(()=>{
-    client12.guilds.get(server).channels.get(channel).send(' -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee')
-    },305);
-})
-client13.on('ready', async() => {
-var server = "509370079046270977"; // ايدي السررفر
-var channel = "511605121344602113";//ايدي الروم
-    setInterval(()=>{
-    client13.guilds.get(server).channels.get(channel).send(' -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee')
-    },305);
-})
-client14.on('ready', async() => {
-var server = "509370079046270977"; // ايدي السررفر
-var channel = "511605121344602113";//ايدي الروم
-    setInterval(()=>{
-    client14.guilds.get(server).channels.get(channel).send(' -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee')
-    },305);
-})
-client15.on('ready', async() => {
-var server = "509370079046270977"; // ايدي السررفر
-var channel = "511605121344602113";//ايدي الروم
-    setInterval(()=>{
-    client15.guilds.get(server).channels.get(channel).send(' -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee')
-    },305);
-})
-client16.on('ready', async() => {
-var server = "509370079046270977"; // ايدي السررفر
-var channel = "511605121344602113";//ايدي الروم
-    setInterval(()=>{
-    client16.guilds.get(server).channels.get(channel).send(' -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee -Mee')
-    },305);
-})
 
 client.login(process.env.BOT_TOKEN);
 client.login(process.env.BOT_TOKEN);
@@ -139,24 +49,3 @@ client2.login(process.env.BOT_TOKEN2);
 client3.login(process.env.BOT_TOKEN3);
 client4.login(process.env.BOT_TOKEN4);
 client5.login(process.env.BOT_TOKEN5);
-client6.login(process.env.BOT_TOKEN6);
-client7.login(process.env.BOT_TOKEN7);
-client8.login(process.env.BOT_TOKEN8);
-client9.login(process.env.BOT_TOKEN9);
-client10.login(process.env.BOT_TOKEN10);
-client11.login(process.env.BOT_TOKEN11);
-client12.login(process.env.BOT_TOKEN12);
-client13.login(process.env.BOT_TOKEN13);
-client14.login(process.env.BOT_TOKEN14);
-client15.login(process.env.BOT_TOKEN15);
-client16.login(process.env.BOT_TOKEN16);
-
-
-
-
-
-
-
-
-
-
